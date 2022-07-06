c88bdf08e080810c42f6d9f02fea80fd5577e652
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-07-05 20:50:53 -0400

    added psuedo code

---
+++ assets/scripts.js
@@ -0,0 +1,55 @@
+// declare our variables for our button, user input, filter options, and toggle switch
+
+
+
+
+
+
+
+
+// an event listener for our search button
+
+
+
+
+// function to fetch and return data
+
+
+
+// function to display the material
+
+
+
+
+
+// a function saying if the toggle switch is not selected then display restaurants in a top 10 list
+
+
+// else display a random choice
+
+
+// function to save and return local storage for previous searches
+
+
+// for each list item clicked display the name, address, contact info, picture and filters chosen
+
+
+
+// function to clear the search info
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
