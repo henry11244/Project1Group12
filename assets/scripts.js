6fada1638dac9339c70785c43d434bd1dc0d2c2e
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-07-08 13:17:07 -0700

    edit pseudocode comments

--- assets/scripts.js
+++ assets/scripts.js
@@ -160,57 +160,30 @@ function filterPrice(data) {
 
 
 // declare our variables for our button, user input, filter options, and toggle switch
-
-
-
-
-
-
-
-
 // an event listener for our search button
-
-
-
-
 // function to fetch and return data
-
-
-
 // function to display the material
-
-
-
-
-
-// a function saying if the toggle switch is not selected then display restaurants in a top 10 list
-
-
-// else display a random choice
-
-
-// function to save and return local storage for previous searches
-
-
-// for each list item clicked display the name, address, contact info, picture and filters chosen
-
-
-
 // function to clear the search info
+// for each list item clicked display the name, address, contact info, picture and filters chosen (future goal?)
+
+
+
+
+// function for filter options
+
+
+
+
+// a function saying if the toggle switch is not selected then display restaurants in a top 10 list else display a random choice (SUFI_)
 
 
 
 
 
+// function to save and return local storage for previous searches (HENRY)
 
 
 
 
 
-
-
-
-
-
-
-
+//display image and link in each restaurant card (CHELSEA)
