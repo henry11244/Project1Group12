e1ebda99eab9928ed4e52faddce8e91b9a738731
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-07-08 17:18:32 -0700

    updated bulma layout in html and css

--- assets/scripts.js
+++ assets/scripts.js
@@ -4,7 +4,6 @@ var count = 0
 
 function apiPull() {
     cityName = userInput.val()
-    // need to insert city search into URL and inject URL IMG
     fetch(`https://api.unsplash.com/search/photos?page=1&query=${cityName}&client_id=0jqDAD-zXewS00iMXPqH9-EWmxQwXtr_3FGl5EqT8c0`)
         .then(response => response.json())
         .then(data => {
